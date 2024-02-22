6a80fdb7dc94e7f93b0c3465e3150771952b013d
Le Viet <levietnee@gmail.com>
2024-02-22 11:32:51 +0700

    start

---
+++ src/sections/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero.vue";
+import PopularProducts from "./PopularProducts.vue";
+import CustomerReview from "./CustomerReview.vue";
+import Services from "./Services.vue";
+import SpecialOffer from "./SpecialOffer.vue";
+import Subcribe from "./Subcribe.vue";
+import SuperQuality from "./SuperQuality.vue";
+import Footer from "./Footer.vue";
+
+export {
+    Hero,
+    PopularProducts,
+    CustomerReview,
+    SpecialOffer,
+    Services,
+    SuperQuality,
+    Subcribe,
+    Footer
+}
